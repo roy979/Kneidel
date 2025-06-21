3eebd02347d0fffbc6181806cb6b02297159a597
Roy-SEdge <roy.waisbord@sonicedge.io>
2025-06-21 20:29:19 +0300

    Kneidel Web

--- Package_Creation/Prepare_Songs.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Get the absolute path of the directory where this batch file is located
-set SCRIPT_DIR=%~dp0
-
-REM Use the virtual environment's Python directly
-"%SCRIPT_DIR%Kneidel_venv\Scripts\python.exe" "%SCRIPT_DIR%Scripts\Prepare_Songs.py"
-
-REM Pause to keep the window open if there's an error
-pause
